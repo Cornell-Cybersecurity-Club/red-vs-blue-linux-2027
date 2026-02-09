45537768f90a1bf2a6a2138add2e4b38d177e36b
Buster Coden <busterc246@proton.me>
2026-02-09 15:53:10 -0500

    Started fixing package scripts

--- setup.sh
+++ setup.sh
@@ -5,27 +5,27 @@ if [ "$(id -u || true)" -ne 0 ]; then
   exit 1
 fi
 
-./tools/backup_create.sh
-./permission_fix.sh
-./tools/users.sh
-./tools/password_rotate.sh
-./permission_fix.sh
-./networking.sh
-./firewall.sh
-./ssh_remove_keys.sh
-./ssh_config.sh
-./package_manager_reset.sh
-./package_reinstall.sh
-./package_install.sh
-./package_remove.sh
-./firewall.sh
-./kernel.sh
-./auditd.sh
-./rsyslog.sh
-./journald.sh
-./apparmor.sh
-./fstab.sh
-./misc.sh
-./file_cleaner.sh
-./dconf.sh
-./tools/password_rotate.sh
+# ./tools/backup_create.sh
+# ./permission_fix.sh
+# ./tools/users.sh
+# ./tools/password_rotate.sh
+# ./permission_fix.sh
+# ./networking.sh
+# ./firewall.sh
+# ./ssh_remove_keys.sh
+# ./ssh_config.sh
+# ./package_manager_reset.sh
+# ./package_reinstall.sh
+# ./package_install.sh
+# ./package_remove.sh
+# ./firewall.sh
+# ./kernel.sh
+# ./auditd.sh
+# ./rsyslog.sh
+# ./journald.sh
+# ./apparmor.sh
+# ./fstab.sh
+# ./misc.sh
+# ./file_cleaner.sh
+# ./dconf.sh
+# ./tools/password_rotate.sh
